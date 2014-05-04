c608653e964299d97e51003e4a55a4444716d623
Mac Kiser <mkiser96@gmail.com>
2014-05-03 23:00:23 -0400

    first commit, still a work in progress

---
+++ LoopsWorksheet.java
@@ -0,0 +1,54 @@
+package Default;
+import java.util.Scanner;
+
+
+public class LoopsWorksheet {
+
+	public static void main(String[] args) {
+
+		/////////// NUMBER 7 ///////////
+
+		int looper = 1;
+
+		for (looper = 1; looper <= 128; looper *= 2) {
+			System.out.println(looper);
+		}
+
+		/////////// NUMBER 8 ///////////
+
+		int sum = 0, inputNum;
+	      int counter;
+	      float mean;
+	      Scanner NumScanner = new Scanner(System.in);
+	      Scanner charScanner = new Scanner(System.in);
+
+	      System.out.println("\nEnter the total number of terms whose mean you want to calculate");
+
+	      counter = NumScanner.nextInt();
+
+	      System.out.println("Please enter " + counter + " numbers:");
+
+	      for(int x = 1; x<=counter ;x++){
+	          inputNum = NumScanner.nextInt();
+	          sum = sum + inputNum;
+	          System.out.println();
+	      }
+
+	      mean = sum / counter;
+	      System.out.println("The mean of the " + counter + " numbers you entered is " + mean);
+
+	      /////////// NUMBER 9 ///////////
+
+	      System.out.println("What is your pets name?");
+
+	      Scanner pet = new Scanner (System.in);
+	      String name = pet.nextLine();
+
+	      System.out.println("How many times would you like your "
+	      		+ "pets name to appear on the screen?");
+	      Scanner num = new Scanner (System.in);
+	      int count = num.nextInt();
+
+	}
+
+}
